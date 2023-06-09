4883886db8d8931da355a0603d887b489f273063
manon-col <manon.collard00@gmail.com>
2023-06-09 17:27:06 +0200

    Update clustering.py

--- clustering.py
+++ clustering.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.linear_model import RANSACRegressor
+from sklearn.pipeline import make_pipeline
 
 class deadwood_detection:
     """
@@ -28,6 +29,7 @@ class deadwood_detection:
         ----------
         las_file : LasData file
             File containing the points to process.
+            
         """
         # File
         self._las_file = las_file
@@ -39,6 +41,8 @@ class deadwood_detection:
         self._labels = None
         # Set of clustering labels
         self._unique_labels = None
+        # List of cluster objects
+        self._clusters = []
         # Filename without extension
         self._filename = os.path.splitext(os.path.basename(self._las_file))[0]
     
@@ -69,6 +73,10 @@ class deadwood_detection:
         self._unique_labels = set(self._labels)
         n_clusters = len(self._unique_labels) - (1 if -1 in self._labels else 0)
         n_noise = list(self._labels).count(-1)
+        
+        for label in self._unique_labels:
+            cluster_points = self._data_xyz[self._labels == label]
+            self._clusters.append(cluster(label, cluster_points))
     
         print("Estimated number of clusters: %d" % n_clusters)
         print("Estimated number of noise points: %d" % n_noise)
@@ -127,8 +135,7 @@ class deadwood_detection:
             new_las.add_extra_dim(laspy.ExtraBytesParams(name="cluster",
                                                          type=np.uint8))
             
-            # Browsing all the data to find out which label goes to which point,
-            # and  fill "clusters" field. Must be optimised...
+            # Filling the field
             for index in range(len(self._data_xyz)):
                 new_las.cluster[index] = self._labels[index]+1
             
@@ -138,5 +145,200 @@ class deadwood_detection:
             new_las.write(self._filename+'_clusters.las')
         
         else: print("Please run the clustering method first.")
+        
+ # /!\ the cylinder fitting part is in progress, doesn't work
+ 
+    def fit_in_cluster(self, cluster_points, num_cylinders):
+        """
+        Fit a given number of cylinders in a given cloud points.
+
+        Parameters
+        ----------
+        cluster_points : ndarray
+            Array of points belonging to the cluster.
+        num_cylinders : int
+            Number of cylinders to fit.
+
+        Returns
+        -------
+        cylinder_models : list
+            List containing the fitted cylinder models.
+
+        """
+        
+        cylinder_models = [] # Initialisation
+        
+        # Apply PCA to find the orientation of the points
+        pca = PCA(n_components=3)
+        pca.fit(cluster_points)
+        orientation = pca.components_[0]
+
+        # Fit multiple cylinders using RANSAC
+        for _ in range(num_cylinders):
+
+            # Create RANSAC regressor for cylinder fitting
+            ransac = RANSACRegressor()
+
+            # Create pipeline with PCA and RANSAC
+            pipeline = make_pipeline(PCA(n_components=3), ransac)
+
+            # Fit the pipeline to the cluster points
+            pipeline.fit(cluster_points)
+
+            # Get the inlier indices from RANSAC
+            inlier_indices = ransac.inlier_mask_
+
+            # Extract the inlier points
+            inlier_points = cluster_points[inlier_indices]
+
+            # Fit the cylinder again using all inliers
+            pipeline.fit(inlier_points, inlier_points[:, 2])
+
+             # Store the fitted cylinder model
+            cylinder_model = {
+                'center': pipeline.named_steps['ransacregressor'].estimator_.center_,
+                'direction': orientation,
+                'radius': pipeline.named_steps['ransacregressor'].estimator_.radius_,
+                'height': np.max(inlier_points[:, 2]) - np.min(inlier_points[:, 2])
+            }
+            cylinder_models.append(cylinder_model)
+        
+        return cylinder_models
+
+    def quality_measure(self, cluster_points, cylinder_model):
+        """
+        Calculate the quality measure for the given cylinder model and cluster points.
+        The quality measure is the average distance between the points and the cylinder surface.
     
+        Parameters
+        ----------
+        cluster_points : ndarray
+            Array of points belonging to the cluster.
+        cylinder_model : dict
+            Dictionary containing the cylinder model parameters.
+            Expected keys: 'center', 'direction', 'radius', 'height'
     
+        Returns
+        -------
+        float
+            Quality measure for the cylinder model.
+    
+        """
+    
+        center = cylinder_model['center']
+        direction = cylinder_model['direction']
+        radius = cylinder_model['radius']
+    
+        # Calculate the distance between points and cylinder surface
+        distances = []
+        for point in cluster_points:
+            # Project the point onto the cylinder axis
+            projection = np.dot(point - center, direction) * direction + center
+            # Calculate the distance between the point and the projected point on the axis
+            distance = np.linalg.norm(point - projection) - radius
+            distances.append(distance)
+    
+        # Calculate the average distance as the quality measure
+        quality_measure = np.mean(distances)
+    
+        return quality_measure
+
+    def cylinder_fitting(self):
+        """
+        Fit cylinders into each cluster. Multiple iterations allow to know how
+        many cylinders must be fit.
+
+        """
+        
+        if self._labels is not None:
+            
+            for cluster in self._clusters:
+                
+                # Getting cluster points
+                cluster_points = cluster.get_points()
+                                
+                # Initialise variables
+                best_num_cylinders = 1
+                best_measure = float('inf')
+                
+                # Iterate over different numbers of cylinders
+                for num_cylinders in range(1, 20):
+                    
+                    # Fit cylinders using RANSAC
+                    cylinder_models = self.fit_in_cluster(cluster_points,
+                                                          num_cylinders)
+                    
+                    # Calculate quality measure for the cylinder fitting
+                    measure = self.quality_measure(cluster_points,
+                                                   cylinder_models)
+                    
+                    # Update the best number of cylinders if the measure improves
+                    if measure < best_measure:
+                        best_num_cylinders = num_cylinders
+                    else:
+                        # Stop iterating if the measure starts to degrade
+                        break
+                
+                cylinder_models = self.fit_in_clusters(cluster_points,
+                                                       best_num_cylinders)
+                
+                cluster.set_cylinders(cylinder_models)
+                
+        else: print("Please run the clustering method first.")
+
+class cluster:
+    """
+    Manages a cluster of 3D points.
+    
+    """
+    
+    def __init__(self, label, points):
+        self._label = str(label)
+        self._points = points
+        self._cylinder_models = None
+    
+    def label(self):
+        return(self._label)
+    
+    def get_points(self):
+        return(self._points)
+    
+    def set_cylinders(self, cylinder_models):
+        """
+        Store cylinders fitted to the cluster with the RANSAC method.
+
+        Parameters
+        ----------
+        cylinder_models : list
+            List containing all fitted cylinders.
+        
+        """
+        
+        self._cylinder_models = cylinder_models
+
+
+# Test
+test = deadwood_detection("output_computree.las")
+test.clustering()
+test.cylinder_fitting()
+
+# notes...
+# df= pd.DataFrame(columns=['cluster', 'orientation'])
+# for cluster in test._unique_labels:
+#     if cluster != -1:
+#         cluster_points = test._data_xyz[test._labels == cluster]
+#         pca=PCA(n_components=1)
+#         pca.fit(cluster_points)
+#         orientation = pca.components_[0]
+#         # Normalize the orientation vector
+#         normalized_orientation = orientation / np.linalg.norm(orientation)
+    
+#         # Define the vertical vector
+#         vertical_vector = np.array([0, 0, 1])
+    
+#         # Calculate the dot product between the orientation and vertical vectors
+#         dot_product = np.dot(normalized_orientation, vertical_vector)
+    
+#         # Calculate the angle using arccos
+#         angle = np.degrees(np.arccos(dot_product))
+#         df.loc[len(df)]=[int(cluster),angle]
