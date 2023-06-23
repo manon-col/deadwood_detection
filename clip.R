df2686ac77c00339fec35a89690329d28a5567d9
manon-col <manon.collard00@gmail.com>
2023-06-23 17:12:08 +0200

    Update clip.R
    Allows to clip a point cloud into a circular subset of a given radius (around a known centre).

--- clip.R
+++ clip.R
@@ -6,9 +6,9 @@ spheres_file <- 'spheres_coordinates.csv'
 spheres_data <- read.csv2(spheres_file)
 
 
-# Function which creates a circular subset of a (las) point cloud of a given
-# radius
 clip_las <- function(las_file, radius) {
+  #'Create a circular subset of a given radius around the centre with known
+  #'coordinates within a point cloud.
 
   # Reference = filename without extension
   filename <- tools::file_path_sans_ext(basename(las_file))
@@ -24,22 +24,21 @@ clip_las <- function(las_file, radius) {
   plot <- clip_circle(las_data, x = centre_x, y = centre_y, r = radius)
   
   # Out file path
-  out <- paste(filename, "_clip.las")
+  out <- paste("scans/", filename, "_clip.las", sep = "")
   
   # Saving out file
   writeLAS(plot, out)
 }
 
 
-# Listing all las files
-las_files <- list.files(pattern = "\\.las$", full.names = TRUE)
+# Listing las files
+las_files <- list.files(path = "scans/", pattern = "\\.las$", full.names = TRUE)
 
-# Cliping las files
+# Clipping all las files
 for (las_file in las_files) {
   
   # Checking if the file isn't already clipped
-  expected_file <- paste(tools::file_path_sans_ext(basename(las_file)),
-                         "_clip.las")
+  expected_file <- paste(tools::file_path_sans_ext(basename(las_file)), "_clip.las", sep = "")
   
   if (!file.exists(expected_file)) {
     clip_las(las_file, radius = 22)
