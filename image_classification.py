a4e765a4521a0f4ef43bdc605f3b23c1de904df2
manon-col <manon.collard00@gmail.com>
2023-07-03 17:30:29 +0200

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -4,77 +4,399 @@
 @author: manon-col
 """
 
+import os
 import tensorflow as tf
-import matplotlib.pyplot as plt
-from tensorflow.keras import layers, models
+from tensorflow import keras
+from tensorflow.keras import layers
 
 
-directory = 'CNN_data/07_04' # location of image data to train the model
+image_size = (217, 217)
 
-# Generate dataset
+train_path = 'CNN_data/model_data'
+deadwood_path = train_path + '/deadwood'
+other_path = train_path + '/other'
+unlabelled_path = train_path + '/unlabelled'
+test_path = 'CNN_data/model_data'
 
-image_size = (217, 217) # dimension of image in pixels
-num_classes = 2 # number of classes in the dataset
-batch_size = 32
 
-train_ds, val_ds = tf.keras.utils.image_dataset_from_directory(
-    directory,
-    validation_split=0.2,
-    subset="both",
-    seed=42,
-    image_size=image_size,
-    batch_size=batch_size,
-)
+# =============================================================================
+# Hyperparameters
+# =============================================================================
 
-# Image data augmentation : apply random transformations to artificially
-# introduce samples
+AUTOTUNE = tf.data.AUTOTUNE
+shuffle_buffer = 5000
 
-data_augmentation = tf.keras.Sequential(
-    [
-        layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
-        layers.experimental.preprocessing.RandomRotation(0.1),
+labelled_train_images = len([file for file in os.listdir(deadwood_path)]) + \
+    len([file for file in os.listdir(other_path)])
+unlabelled_images = len([file for file in os.listdir(unlabelled_path)])
+
+temperature = 0.1
+queue_size = 10000
+contrastive_augmenter = {
+    "brightness": 0.5,
+    "name": "contrastive_augmenter",
+    "scale": (0.2, 1.0),
+}
+classification_augmenter = {
+    "brightness": 0.2,
+    "name": "classification_augmenter",
+    "scale": (0.5, 1.0),
+}
+input_shape = (image_size[0], image_size[1], 3)
+width = 128
+num_epochs = 25
+steps_per_epoch = 200
+
+
+def prepare_dataset():
+    
+    unlabelled_batch_size = unlabelled_images // steps_per_epoch
+    labelled_batch_size = labelled_train_images // steps_per_epoch
+    batch_size = unlabelled_batch_size + labelled_batch_size
+    
+    unlabeled_batch_size = unlabelled_images // steps_per_epoch
+    labeled_batch_size = labelled_train_images // steps_per_epoch
+    batch_size = unlabeled_batch_size + labeled_batch_size
+
+    unlabelled_train_dataset = (
+        tf.data.Dataset.list_files(unlabelled_path)
+        .shuffle(buffer_size=shuffle_buffer)
+        .batch(unlabelled_batch_size, drop_remainder=True)
+    )
+    labelled_train_dataset = (
+        tf.data.Dataset.list_files(deadwood_path)
+        .concatenate(tf.data.Dataset.list_files(other_path))
+        .shuffle(buffer_size=shuffle_buffer)
+        .batch(labelled_batch_size, drop_remainder=True)
+    )
+    test_dataset = (
+        tf.data.Dataset.list_files(test_path)
+        .batch(batch_size)
+        .prefetch(buffer_size=AUTOTUNE)
+    )
+    train_dataset = tf.data.Dataset.zip(
+        (unlabelled_train_dataset, unlabelled_train_dataset)
+    ).prefetch(buffer_size=AUTOTUNE)
+
+    return batch_size, train_dataset, labelled_train_dataset, test_dataset
+
+batch_size, train_dataset, labeled_train_dataset, test_dataset = prepare_dataset()
+
+class RandomResizedCrop(layers.Layer):
+    
+    def __init__(self, scale, ratio):
+        super().__init__()
+        self.scale = scale
+        self.log_ratio = (tf.math.log(ratio[0]), tf.math.log(ratio[1]))
+
+    def call(self, images):
         
-    ]
+        batch_size = tf.shape(images)[0]
+        height = tf.shape(images)[1]
+        width = tf.shape(images)[2]
+
+        random_scales = tf.random.uniform((batch_size,), self.scale[0], self.scale[1])
+        random_ratios = tf.exp(
+            tf.random.uniform((batch_size,), self.log_ratio[0], self.log_ratio[1])
+        )
+
+        new_heights = tf.clip_by_value(tf.sqrt(random_scales / random_ratios), 0, 1)
+        new_widths = tf.clip_by_value(tf.sqrt(random_scales * random_ratios), 0, 1)
+        height_offsets = tf.random.uniform((batch_size,), 0, 1 - new_heights)
+        width_offsets = tf.random.uniform((batch_size,), 0, 1 - new_widths)
+
+        bounding_boxes = tf.stack(
+            [
+                height_offsets,
+                width_offsets,
+                height_offsets + new_heights,
+                width_offsets + new_widths,
+            ],
+            axis=1,
+        )
+        images = tf.image.crop_and_resize(
+            images, bounding_boxes, tf.range(batch_size), (height, width)
+        )
+        return images
+
+
+class RandomBrightness(layers.Layer):
+    
+    def __init__(self, brightness):
+        super().__init__()
+        self.brightness = brightness
+
+    def blend(self, images_1, images_2, ratios):
+        return tf.clip_by_value(ratios * images_1 + (1.0 - ratios) * images_2, 0, 1)
+
+    def random_brightness(self, images):
+        # random interpolation/extrapolation between the image and darkness
+        return self.blend(
+            images,
+            0,
+            tf.random.uniform(
+                (tf.shape(images)[0], 1, 1, 1), 1 - self.brightness, 1 + self.brightness
+            ),
+        )
+
+    def call(self, images):
+        
+        images = self.random_brightness(images)
+        return images
+
+
+def augmenter(brightness, name, scale):
+    
+    return keras.Sequential(
+        [
+            layers.Input(shape=input_shape),
+            layers.Rescaling(1 / 255),
+            layers.RandomFlip("horizontal"),
+            RandomResizedCrop(scale=scale, ratio=(3 / 4, 4 / 3)),
+            RandomBrightness(brightness=brightness),
+        ],
+        name=name,
+    )
+
+
+def encoder():
+    return keras.Sequential(
+        [
+            layers.Input(shape=input_shape),
+            layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            layers.Flatten(),
+            layers.Dense(width, activation="relu"),
+        ],
+        name="encoder",
+    )
+
+
+class NNCLR(keras.Model):
+    
+    def __init__(
+        self, temperature, queue_size,
+    ):
+        super().__init__()
+        self.probe_accuracy = keras.metrics.SparseCategoricalAccuracy()
+        self.correlation_accuracy = keras.metrics.SparseCategoricalAccuracy()
+        self.contrastive_accuracy = keras.metrics.SparseCategoricalAccuracy()
+        self.probe_loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+        self.contrastive_augmenter = augmenter(**contrastive_augmenter)
+        self.classification_augmenter = augmenter(**classification_augmenter)
+        self.encoder = encoder()
+        self.projection_head = keras.Sequential(
+            [
+                layers.Input(shape=(width,)),
+                layers.Dense(width, activation="relu"),
+                layers.Dense(width),
+            ],
+            name="projection_head",
+        )
+        self.linear_probe = keras.Sequential(
+            [layers.Input(shape=(width,)), layers.Dense(10)], name="linear_probe"
+        )
+        self.temperature = temperature
+
+        feature_dimensions = self.encoder.output_shape[1]
+        self.feature_queue = tf.Variable(
+            tf.math.l2_normalize(
+                tf.random.normal(shape=(queue_size, feature_dimensions)), axis=1
+            ),
+            trainable=False,
+        )
+
+    def compile(self, contrastive_optimizer, probe_optimizer, **kwargs):
+        super().compile(**kwargs)
+        self.contrastive_optimizer = contrastive_optimizer
+        self.probe_optimizer = probe_optimizer
+
+    def nearest_neighbour(self, projections):
+        support_similarities = tf.matmul(
+            projections, self.feature_queue, transpose_b=True
+        )
+        nn_projections = tf.gather(
+            self.feature_queue, tf.argmax(support_similarities, axis=1), axis=0
+        )
+        return projections + tf.stop_gradient(nn_projections - projections)
+
+    def update_contrastive_accuracy(self, features_1, features_2):
+        features_1 = tf.math.l2_normalize(features_1, axis=1)
+        features_2 = tf.math.l2_normalize(features_2, axis=1)
+        similarities = tf.matmul(features_1, features_2, transpose_b=True)
+
+        batch_size = tf.shape(features_1)[0]
+        contrastive_labels = tf.range(batch_size)
+        self.contrastive_accuracy.update_state(
+            tf.concat([contrastive_labels, contrastive_labels], axis=0),
+            tf.concat([similarities, tf.transpose(similarities)], axis=0),
+        )
+
+    def update_correlation_accuracy(self, features_1, features_2):
+        features_1 = (
+            features_1 - tf.reduce_mean(features_1, axis=0)
+        ) / tf.math.reduce_std(features_1, axis=0)
+        features_2 = (
+            features_2 - tf.reduce_mean(features_2, axis=0)
+        ) / tf.math.reduce_std(features_2, axis=0)
+
+        batch_size = tf.shape(features_1, out_type=tf.float32)[0]
+        cross_correlation = (
+            tf.matmul(features_1, features_2, transpose_a=True) / batch_size
+        )
+
+        feature_dim = tf.shape(features_1)[1]
+        correlation_labels = tf.range(feature_dim)
+        self.correlation_accuracy.update_state(
+            tf.concat([correlation_labels, correlation_labels], axis=0),
+            tf.concat([cross_correlation, tf.transpose(cross_correlation)], axis=0),
+        )
+
+    def contrastive_loss(self, projections_1, projections_2):
+        projections_1 = tf.math.l2_normalize(projections_1, axis=1)
+        projections_2 = tf.math.l2_normalize(projections_2, axis=1)
+
+        similarities_1_2_1 = (
+            tf.matmul(
+                self.nearest_neighbour(projections_1), projections_2, transpose_b=True
+            )
+            / self.temperature
+        )
+        similarities_1_2_2 = (
+            tf.matmul(
+                projections_2, self.nearest_neighbour(projections_1), transpose_b=True
+            )
+            / self.temperature
+        )
+
+        similarities_2_1_1 = (
+            tf.matmul(
+                self.nearest_neighbour(projections_2), projections_1, transpose_b=True
+            )
+            / self.temperature
+        )
+        similarities_2_1_2 = (
+            tf.matmul(
+                projections_1, self.nearest_neighbour(projections_2), transpose_b=True
+            )
+            / self.temperature
+        )
+
+        batch_size = tf.shape(projections_1)[0]
+        contrastive_labels = tf.range(batch_size)
+        loss = keras.losses.sparse_categorical_crossentropy(
+            tf.concat(
+                [
+                    contrastive_labels,
+                    contrastive_labels,
+                    contrastive_labels,
+                    contrastive_labels,
+                ],
+                axis=0,
+            ),
+            tf.concat(
+                [
+                    similarities_1_2_1,
+                    similarities_1_2_2,
+                    similarities_2_1_1,
+                    similarities_2_1_2,
+                ],
+                axis=0,
+            ),
+            from_logits=True,
+        )
+
+        self.feature_queue.assign(
+            tf.concat([projections_1, self.feature_queue[:-batch_size]], axis=0)
+        )
+        return loss
+
+    def train_step(self, data):
+        
+        (unlabeled_images, _), (labeled_images, labels) = data
+        images = tf.concat((unlabeled_images, labeled_images), axis=0)
+        augmented_images_1 = self.contrastive_augmenter(images)
+        augmented_images_2 = self.contrastive_augmenter(images)
+
+        with tf.GradientTape() as tape:
+            features_1 = self.encoder(augmented_images_1)
+            features_2 = self.encoder(augmented_images_2)
+            projections_1 = self.projection_head(features_1)
+            projections_2 = self.projection_head(features_2)
+            contrastive_loss = self.contrastive_loss(projections_1, projections_2)
+        gradients = tape.gradient(
+            contrastive_loss,
+            self.encoder.trainable_weights + self.projection_head.trainable_weights,
+        )
+        self.contrastive_optimizer.apply_gradients(
+            zip(
+                gradients,
+                self.encoder.trainable_weights + self.projection_head.trainable_weights,
+            )
+        )
+        self.update_contrastive_accuracy(features_1, features_2)
+        self.update_correlation_accuracy(features_1, features_2)
+        preprocessed_images = self.classification_augmenter(labeled_images)
+
+        with tf.GradientTape() as tape:
+            features = self.encoder(preprocessed_images)
+            class_logits = self.linear_probe(features)
+            probe_loss = self.probe_loss(labels, class_logits)
+        gradients = tape.gradient(probe_loss, self.linear_probe.trainable_weights)
+        self.probe_optimizer.apply_gradients(
+            zip(gradients, self.linear_probe.trainable_weights)
+        )
+        self.probe_accuracy.update_state(labels, class_logits)
+
+        return {
+            "c_loss": contrastive_loss,
+            "c_acc": self.contrastive_accuracy.result(),
+            "r_acc": self.correlation_accuracy.result(),
+            "p_loss": probe_loss,
+            "p_acc": self.probe_accuracy.result(),
+        }
+
+    def test_step(self, data):
+        labeled_images, labels = data
+
+        preprocessed_images = self.classification_augmenter(
+            labeled_images, training=False
+        )
+        features = self.encoder(preprocessed_images, training=False)
+        class_logits = self.linear_probe(features, training=False)
+        probe_loss = self.probe_loss(labels, class_logits)
+
+        self.probe_accuracy.update_state(labels, class_logits)
+        return {"p_loss": probe_loss, "p_acc": self.probe_accuracy.result()}
+
+
+model = NNCLR(temperature=temperature, queue_size=queue_size)
+model.compile(
+    contrastive_optimizer=keras.optimizers.Adam(),
+    probe_optimizer=keras.optimizers.Adam(),
+)
+pretrain_history = model.fit(
+    train_dataset, epochs=num_epochs, validation_data=test_dataset
 )
 
-# # Dataset with augmented data gen
-train_ds_augmented = train_ds.map(lambda x, y: (data_augmentation(x, training=True), y))
+finetuning_model = keras.Sequential(
+    [
+        layers.Input(shape=input_shape),
+        augmenter(**classification_augmenter),
+        model.encoder,
+        layers.Dense(10),
+    ],
+    name="finetuning_model",
+)
+finetuning_model.compile(
+    optimizer=keras.optimizers.Adam(),
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[keras.metrics.SparseCategoricalAccuracy(name="acc")],
+)
 
-# Create a convolutional base
-
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu',
-                        input_shape=(image_size[0], image_size[1], 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-
-# Add layers on top
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(num_classes))
-
-# Compile the model
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-
-# Train the model
-epochs = 50  # You can adjust the number of epochs as needed
-history = model.fit(train_ds_augmented,
-                    epochs=epochs,
-                    validation_data=val_ds)
-
-# Display learning curves
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label='val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
-plt.legend(loc='lower right')
-plt.show()
-
-# Model evaluation
-test_loss, test_acc = model.evaluate(val_ds, verbose=2)
-print(test_acc)
+finetuning_history = finetuning_model.fit(
+    labeled_train_dataset, epochs=num_epochs, validation_data=test_dataset
+)
