7d00eecbee1f3ea21d8c3d15a9a19e34ce94a27e
manon-col <manon.collard00@gmail.com>
2023-08-01 16:15:36 +0200

    v1.4
    Augmenter + encoder update

--- image_classification.py
+++ image_classification.py
@@ -33,15 +33,11 @@ class Model:
         self._queue_size = 10000
     
         self._contrastive_augmenter = {
-            "brightness": 0.5,
             "name": "contrastive_augmenter",
-            "scale": (0.2, 1.0),
         }
         
         self._classification_augmenter = {
-            "brightness": 0.2,
             "name": "classification_augmenter",
-            "scale": (0.5, 1.0),
         }
         
         self._width = 128
@@ -295,79 +291,24 @@ class Model:
                     deadwood_images.append(images[i])
         
         return deadwood_images
-        
-
-class RandomResizedCrop(layers.Layer):
-    
-    def __init__(self, scale, ratio):
-        super().__init__()
-        self.scale = scale
-        self.log_ratio = (tf.math.log(ratio[0]), tf.math.log(ratio[1]))
-
-    def call(self, images):
-        batch_size = tf.shape(images)[0]
-        height = tf.shape(images)[1]
-        width = tf.shape(images)[2]
-
-        random_scales = tf.random.uniform((batch_size,), self.scale[0], self.scale[1])
-        random_ratios = tf.exp(
-            tf.random.uniform((batch_size,), self.log_ratio[0], self.log_ratio[1])
-        )
-
-        new_heights = tf.clip_by_value(tf.sqrt(random_scales / random_ratios), 0, 1)
-        new_widths = tf.clip_by_value(tf.sqrt(random_scales * random_ratios), 0, 1)
-        height_offsets = tf.random.uniform((batch_size,), 0, 1 - new_heights)
-        width_offsets = tf.random.uniform((batch_size,), 0, 1 - new_widths)
-
-        bounding_boxes = tf.stack(
-            [
-                height_offsets,
-                width_offsets,
-                height_offsets + new_heights,
-                width_offsets + new_widths,
-            ],
-            axis=1,
-        )
-        images = tf.image.crop_and_resize(
-            images, bounding_boxes, tf.range(batch_size), (height, width)
-        )
-        return images
 
 
-class RandomBrightness(layers.Layer):
-    
-    def __init__(self, brightness):
-        super().__init__()
-        self.brightness = brightness
-
-    def blend(self, images_1, images_2, ratios):
-        return tf.clip_by_value(ratios * images_1 + (1.0 - ratios) * images_2, 0, 1)
-
-    def random_brightness(self, images):
-        # random interpolation/extrapolation between the image and darkness
-        return self.blend(
-            images,
-            0,
-            tf.random.uniform(
-                (tf.shape(images)[0], 1, 1, 1), 1 - self.brightness, 1 + self.brightness
-            ),
-        )
-
-    def call(self, images):
-        
-        images = self.random_brightness(images)
-        return images
-
-
-def augmenter(brightness, name, scale, input_shape):
+def augmenter(name, input_shape):
     
     return keras.Sequential(
         [
             layers.Input(shape=input_shape),
             layers.Rescaling(1 / 255),
             layers.RandomFlip("horizontal"),
-            RandomResizedCrop(scale=scale, ratio=(3 / 4, 4 / 3)),
-            RandomBrightness(brightness=brightness),
+            layers.RandomRotation(
+                # Rotation between -10° and 10°
+                factor=(-10/180.0*3.141592653589793,
+                        10/180.0*3.141592653589793)
+                ),
+            layers.RandomTranslation(
+                height_factor=(-0.2, 0.3),
+                width_factor=(-0.2, 0.3)
+                )
         ],
         name=name,
     )
@@ -378,10 +319,18 @@ def encoder(input_shape, width):
     return keras.Sequential(
         [
             layers.Input(shape=input_shape),
-            layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
-            layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
-            layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
-            layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            layers.Conv2D(width, kernel_size=3, activation="relu"),
+            layers.BatchNormalization(),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(width, kernel_size=3, activation="relu"),
+            layers.BatchNormalization(),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(width, kernel_size=3, activation="relu"),
+            layers.BatchNormalization(),
+            layers.MaxPooling2D(pool_size=(2, 2)),
+            layers.Conv2D(width, kernel_size=3, activation="relu"),
+            layers.BatchNormalization(),
+            layers.MaxPooling2D(pool_size=(2, 2)),
             layers.Flatten(),
             layers.Dense(width, activation="relu"),
         ],
