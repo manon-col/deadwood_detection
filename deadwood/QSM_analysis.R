7cd6a10ed7c4f26b4879766581ff674dfb111fdd
manon-col <manon.collard00@gmail.com>
2023-08-07 16:12:22 +0200

    Update QSM_analysis.R

--- deadwood/QSM_analysis.R
+++ deadwood/QSM_analysis.R
@@ -7,8 +7,9 @@ results_file <- "QSM_results.csv"
 if (!file.exists(results_file)) {
   results <- data.frame(reference = character(), total_length = numeric(),
                         total_volume = numeric(), mean_circ = numeric(),
-                        mean_circ_inv = numeric(), length_ord1 = numeric(),
-                        volume_ord_1 = numeric(), num_orders = numeric())
+                        num_orders = numeric(), length_ord01 = numeric(),
+                        volume_ord01 = numeric(), circ_ord01 = numeric(),
+                        inv_circ_ord01 = numeric())
   
   write.table(results, file = results_file, sep = ";", row.names = FALSE,
               col.names = TRUE)
@@ -27,29 +28,50 @@ qsm_analysis <- function(file) {
   # Get total volume
   total_volume <- (qsm$treedata$TotalVolume[1])*0.001 # converting L into m^3
   
-  # Get circumferences from diameters
-  diameters <- qsm$branch$diameter
-  circumferences <- (diameters*pi)*100 # converting m into cm
+  # Get circumferences from radius
+  radius <- qsm$cylinder$radius
+  circumferences <- radius*2*pi*100 # converting m into cm
   mean_circ <- mean(circumferences)
-  
-  # Get circumferences of branches that can be inventoried
-  inv_circumferences <- circumferences[circumferences >= 20]
-  mean_circ_inv <- mean(inv_circumferences)
-  
-  # Get length and volume of 1st order branch
-  length_ord1 <- qsm$treedata$LenBranchOrd[1]
-  volume_ord1 <- (qsm$treedata$VolBranchOrd[1])*0.001
   
   # Get number of branch orders
   num_orders <- qsm$treedata$MaxBranchOrder[1]
   
+  # Get length and volume of branches of order 0 (trunk) and 1
+  length_ord01 <- qsm$treedata$LenBranchOrd[1]+qsm$treedata$LenBranchOrd[2]
+  volume_ord01 <- (qsm$treedata$VolBranchOrd[1]+qsm$treedata$VolBranchOrd[2])*0.001
+  
+  # Get cylinders of order 0 and 1
+  cyl_orders <- qsm$cylinder$BranchOrder
+  cylinders <- which((cyl_orders)<=1)
+  
+  # Get circumferences of orders 0 and 1
+  circs <- c()
+  
+  # Circumferences in inventory range (>= 20cm)
+  inv_circs <- c()
+  
+  for (cyl in cylinders)  {
+    
+    circ <- qsm$cylinder$radius[cyl]*2*pi*100 # metres into centimetres
+    circs <-c(circs, circ)
+    
+    if (circ >= 20) {
+      inv_circs <- c(inv_circs, circ)
+    }
+  }
+  
+  # Get mean circumference for orders 0 and 1
+  circ_ord01 <- mean(circs)
+  inv_circ_ord01 <- mean(inv_circs)
+
   results_data <- read.csv2(results_file)
   
   # Write results
   if (!(reference %in% results_data$reference)) { # to avoid duplication
 
     row <- data.frame(reference, total_length, total_volume, mean_circ,
-                      mean_circ_inv, length_ord1, volume_ord1, num_orders)
+                      num_orders, length_ord01, volume_ord01, circ_ord01,
+                      inv_circ_ord01)
     
     write.table(row, file=results_file, sep=";", append = TRUE, quote = FALSE,
                 col.names = FALSE, row.names = FALSE)
@@ -58,7 +80,7 @@ qsm_analysis <- function(file) {
 
 
 # List .mat files
-qsm_files <- list.files(path = "results/", pattern = "\\.mat$",
+qsm_files <- list.files(path = "QSMs/", pattern = "\\.mat$",
                         full.names = TRUE)
 
 # Perform QSM analysis on all .mat files
